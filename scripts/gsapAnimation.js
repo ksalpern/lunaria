aca32a7d448069848420ea9df11a94c847950dd8
Kseniia P <ksenia.pidopryhora@gmail.com>
2023-01-07 08:36:45 +0200

    add footer

---
+++ scripts/gsapAnimation.js
@@ -0,0 +1,5 @@
+// const tl = gsap.timeline({ defaults: { duration: .5 } });
+// tl.from('.message1', { opacity: 0, y: 30 })
+//   .from('.message2', { opacity: 0, y: 30 })
+//   .from('.banner__message button', { opacity: 0, y: 30 })
+//   .add(btnAnimation);
