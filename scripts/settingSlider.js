31b7671c52f90161215599c6eede0765a9e64b11
Kseniia P <ksenia.pidopryhora@gmail.com>
2023-01-08 23:48:05 +0200

    add projects slider

--- scripts/settingSlider.js
+++ scripts/settingSlider.js
@@ -25,4 +25,29 @@ $(document).ready(function(){
     },
   ]
   });
+  $('.projects__slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 300,
+    responsive: [
+    {
+      breakpoint: 1500,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
+  });
 });
