8a55c1d1e856bbd7377944e8b60366e3749752a0
Kseniia P <ksenia.pidopryhora@gmail.com>
2023-01-09 02:38:45 +0200

    update responsive slider

--- scripts/settingSlider.js
+++ scripts/settingSlider.js
@@ -1,53 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.values__slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     speed: 300,
-    arrows : false,
+    arrows: false,
     responsive: [
-    {
-      breakpoint: 1500,
-      settings: {
-        slidesToShow: 3,
-      }
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-      }
-    },
-  ]
+      {
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
   });
   $('.projects__slider').slick({
     slidesToShow: 2,
     slidesToScroll: 1,
     speed: 300,
+    // infinite: false,
     responsive: [
-    {
-      breakpoint: 1500,
-      settings: {
-        slidesToShow: 3,
-      }
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-      }
-    },
-  ]
+      {
+        breakpoint: 980,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
   });
 });
