fb37849322b99a90ff75434b2b0ff3676b8843d2
Kseniia P <ksenia.pidopryhora@gmail.com>
2023-01-10 08:13:41 +0200

    update

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,35 +1,30 @@
-//this is the button
-let acc = document.getElementsByClassName("accordion-item-header");
+let clickAccordion = document.getElementsByClassName("accordion-item-header");
 
-for (let i = 0; i < acc.length; i++) {
+for (let i = 0; i < clickAccordion.length; i++) {
   //when one of the buttons are clicked run this function
-  acc[i].onclick = function () {
+  clickAccordion[i].onclick = function () {
     //letiables
     let panel = this.nextElementSibling;
     let coursePanel = document.getElementsByClassName("accordion-item-body");
     let courseAccordion = document.getElementsByClassName("accordion-item-header");
     let courseAccordionActive = document.getElementsByClassName("accordion-item-header active");
 
-    /*if pannel is already open - minimize*/
     if (panel.style.maxHeight) {
-      //minifies current pannel if already open
       panel.style.maxHeight = null;
-      //removes the 'active' class as toggle didnt work on browsers minus chrome
       this.classList.remove("active");
-    } else { //pannel isnt open...
+    } else {
       //goes through the buttons and removes the 'active' css (+ and -)
-      for (let ii = 0; ii < courseAccordionActive.length; ii++) {
-        courseAccordionActive[ii].classList.remove("active");
+      for (let j = 0; j < courseAccordionActive.length; j++) {
+        courseAccordionActive[j].classList.remove("active");
       }
-      //Goes through and removes 'activ' from the css, also minifies any 'panels' that might be open
-      for (let iii = 0; iii < coursePanel.length; iii++) {
+      //Goes through and removes 'active' from the css, also minifies any 'panels' that might be open
+      for (let k = 0; k < coursePanel.length; k++) {
         this.classList.remove("active");
-        coursePanel[iii].style.maxHeight = null;
+        coursePanel[k].style.maxHeight = null;
       }
       //opens the specified pannel
       panel.style.maxHeight = panel.scrollHeight + "px";
-      //adds the 'active' addition to the css.
       this.classList.add("active");
     }
-  }//closing to the acc onclick function
-}//closing to the for loop.
+  }
+}
--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,134 +1,242 @@
-const cursor = document.getElementById('cursor');
-document.onmousemove = function (e) {
-  cursor.style.left = (e.pageX - 50) + 'px';
-  cursor.style.top = (e.pageY - 50) + 'px';
-  cursor.style.display = 'block';
-}
-
-// var cursor = {
-//   delay: 8,
-//   _x: 0,
-//   _y: 0,
-//   endX: (window.innerWidth / 2),
-//   endY: (window.innerHeight / 2),
-//   cursorVisible: true,
-//   cursorEnlarged: false,
-//   $dot: document.querySelector('.cursor-dot'),
-//   $outline: document.querySelector('.cursor-dot-outline'),
-  
-//   init: function() {
-//       // Set up element sizes
-//       this.dotSize = this.$dot.offsetWidth;
-//       this.outlineSize = this.$outline.offsetWidth;
-      
-//       this.setupEventListeners();
-//       this.animateDotOutline();
-//   },
-  
-// //     updateCursor: function(e) {
-// //         var self = this;
-      
-// //         console.log(e)
-      
-// //         // Show the cursor
-// //         self.cursorVisible = true;
-// //         self.toggleCursorVisibility();
-
-// //         // Position the dot
-// //         self.endX = e.pageX;
-// //         self.endY = e.pageY;
-// //         self.$dot.style.top = self.endY + 'px';
-// //         self.$dot.style.left = self.endX + 'px';
-// //     },
-  
-//   setupEventListeners: function() {
-//       var self = this;
-      
-//       // Anchor hovering
-//       document.querySelectorAll('a').forEach(function(el) {
-//           el.addEventListener('mouseover', function() {
-//               self.cursorEnlarged = true;
-//               self.toggleCursorSize();
-//           });
-//           el.addEventListener('mouseout', function() {
-//               self.cursorEnlarged = false;
-//               self.toggleCursorSize();
-//           });
-//       });
-      
-//       // Click events
-//       document.addEventListener('mousedown', function() {
-//           self.cursorEnlarged = true;
-//           self.toggleCursorSize();
-//       });
-//       document.addEventListener('mouseup', function() {
-//           self.cursorEnlarged = false;
-//           self.toggleCursorSize();
-//       });
-
-
-//       document.addEventListener('mousemove', function(e) {
-//           // Show the cursor
-//           self.cursorVisible = true;
-//           self.toggleCursorVisibility();
-
-//           // Position the dot
-//           self.endX = e.pageX;
-//           self.endY = e.pageY;
-//           self.$dot.style.top = self.endY + 'px';
-//           self.$dot.style.left = self.endX + 'px';
-//       });
-      
-//       // Hide/show cursor
-//       document.addEventListener('mouseenter', function(e) {
-//           self.cursorVisible = true;
-//           self.toggleCursorVisibility();
-//           self.$dot.style.opacity = 1;
-//           self.$outline.style.opacity = 1;
-//       });
-      
-//       document.addEventListener('mouseleave', function(e) {
-//           self.cursorVisible = true;
-//           self.toggleCursorVisibility();
-//           self.$dot.style.opacity = 0;
-//           self.$outline.style.opacity = 0;
-//       });
-//   },
-  
-//   animateDotOutline: function() {
-//       var self = this;
-      
-//       self._x += (self.endX - self._x) / self.delay;
-//       self._y += (self.endY - self._y) / self.delay;
-//       self.$outline.style.top = self._y + 'px';
-//       self.$outline.style.left = self._x + 'px';
-      
-//       requestAnimationFrame(this.animateDotOutline.bind(self));
-//   },
-  
-//   toggleCursorSize: function() {
-//       var self = this;
-      
-//       if (self.cursorEnlarged) {
-//           self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
-//           self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
-//       } else {
-//           self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
-//           self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
-//       }
-//   },
-  
-//   toggleCursorVisibility: function() {
-//       var self = this;
-      
-//       if (self.cursorVisible) {
-//           self.$dot.style.opacity = 1;
-//           self.$outline.style.opacity = 1;
-//       } else {
-//           self.$dot.style.opacity = 0;
-//           self.$outline.style.opacity = 0;
-//       }
-//   }
+// const cursor = document.getElementById('cursor');
+// document.onmousemove = function (e) {
+//   cursor.style.left = (e.pageX - 50) + 'px';
+//   cursor.style.top = (e.pageY - 50) + 'px';
+//   cursor.style.display = 'block';
 // }
 
-// cursor.init();
+// // var cursor = {
+// //   delay: 8,
+// //   _x: 0,
+// //   _y: 0,
+// //   endX: (window.innerWidth / 2),
+// //   endY: (window.innerHeight / 2),
+// //   cursorVisible: true,
+// //   cursorEnlarged: false,
+// //   $dot: document.querySelector('.cursor-dot'),
+// //   $outline: document.querySelector('.cursor-dot-outline'),
+
+// //   init: function() {
+// //       // Set up element sizes
+// //       this.dotSize = this.$dot.offsetWidth;
+// //       this.outlineSize = this.$outline.offsetWidth;
+
+// //       this.setupEventListeners();
+// //       this.animateDotOutline();
+// //   },
+
+// // //     updateCursor: function(e) {
+// // //         var self = this;
+
+// // //         console.log(e)
+
+// // //         // Show the cursor
+// // //         self.cursorVisible = true;
+// // //         self.toggleCursorVisibility();
+
+// // //         // Position the dot
+// // //         self.endX = e.pageX;
+// // //         self.endY = e.pageY;
+// // //         self.$dot.style.top = self.endY + 'px';
+// // //         self.$dot.style.left = self.endX + 'px';
+// // //     },
+
+// //   setupEventListeners: function() {
+// //       var self = this;
+
+// //       // Anchor hovering
+// //       document.querySelectorAll('a').forEach(function(el) {
+// //           el.addEventListener('mouseover', function() {
+// //               self.cursorEnlarged = true;
+// //               self.toggleCursorSize();
+// //           });
+// //           el.addEventListener('mouseout', function() {
+// //               self.cursorEnlarged = false;
+// //               self.toggleCursorSize();
+// //           });
+// //       });
+
+// //       // Click events
+// //       document.addEventListener('mousedown', function() {
+// //           self.cursorEnlarged = true;
+// //           self.toggleCursorSize();
+// //       });
+// //       document.addEventListener('mouseup', function() {
+// //           self.cursorEnlarged = false;
+// //           self.toggleCursorSize();
+// //       });
+
+
+// //       document.addEventListener('mousemove', function(e) {
+// //           // Show the cursor
+// //           self.cursorVisible = true;
+// //           self.toggleCursorVisibility();
+
+// //           // Position the dot
+// //           self.endX = e.pageX;
+// //           self.endY = e.pageY;
+// //           self.$dot.style.top = self.endY + 'px';
+// //           self.$dot.style.left = self.endX + 'px';
+// //       });
+
+// //       // Hide/show cursor
+// //       document.addEventListener('mouseenter', function(e) {
+// //           self.cursorVisible = true;
+// //           self.toggleCursorVisibility();
+// //           self.$dot.style.opacity = 1;
+// //           self.$outline.style.opacity = 1;
+// //       });
+
+// //       document.addEventListener('mouseleave', function(e) {
+// //           self.cursorVisible = true;
+// //           self.toggleCursorVisibility();
+// //           self.$dot.style.opacity = 0;
+// //           self.$outline.style.opacity = 0;
+// //       });
+// //   },
+
+// //   animateDotOutline: function() {
+// //       var self = this;
+
+// //       self._x += (self.endX - self._x) / self.delay;
+// //       self._y += (self.endY - self._y) / self.delay;
+// //       self.$outline.style.top = self._y + 'px';
+// //       self.$outline.style.left = self._x + 'px';
+
+// //       requestAnimationFrame(this.animateDotOutline.bind(self));
+// //   },
+
+// //   toggleCursorSize: function() {
+// //       var self = this;
+
+// //       if (self.cursorEnlarged) {
+// //           self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
+// //           self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
+// //       } else {
+// //           self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
+// //           self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+// //       }
+// //   },
+
+// //   toggleCursorVisibility: function() {
+// //       var self = this;
+
+// //       if (self.cursorVisible) {
+// //           self.$dot.style.opacity = 1;
+// //           self.$outline.style.opacity = 1;
+// //       } else {
+// //           self.$dot.style.opacity = 0;
+// //           self.$outline.style.opacity = 0;
+// //       }
+// //   }
+// // }
+
+// // cursor.init();
+
+
+
+
+
+
+//===========================================
+// After update OptimizedHTML5
+let cx, cy, mouseX, mouseY, posX, posY, clientX, clientY, dx, dy, tiltx, tilty, request, radius, degree
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Custom JS
+
+  const body = document.querySelector('body')
+
+  cx = window.innerWidth / 2
+  cy = window.innerHeight / 2
+
+  body.addEventListener('mousemove', e => {
+
+    clientX = e.pageX
+    clientY = e.pageY
+
+    request = requestAnimationFrame(updateMe)
+
+    mouseCoords(e)
+    cursor.classList.remove('hidden')
+    follower.classList.remove('hidden')
+
+  })
+
+  function updateMe() {
+
+    dx = clientX - cx
+    dy = clientY - cy
+    tiltx = dy / cy
+    tilty = dx / cx
+    radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2))
+    degree = radius * 12
+    gsap.to('.content', 1, {
+      transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )`
+    })
+
+  }
+
+  
+
+  const cursor = document.getElementById('cursor')
+  const follower = document.getElementById('aura')
+  const links = document.getElementsByTagName('a')
+
+  mouseX = 0, mouseY = 0, posX = 0, posY = 0
+
+  function mouseCoords(e) {
+
+    mouseX = e.pageX
+    mouseY = e.pageY
+
+  }
+
+  gsap.to({}, .01, {
+
+    repeat: -1,
+
+    onRepeat: () => {
+
+      posX += (mouseX - posX) / 5
+      posY += (mouseY - posY) / 5
+
+      gsap.set(cursor, {
+        css: {
+          left: mouseX,
+          top: mouseY
+        }
+      })
+
+      gsap.set(follower, {
+        css: {
+          left: posX - 21,
+          top: posY - 21
+        }
+      })
+
+    }
+
+  })
+
+  for (let i = 0; i < links.length; i++) {
+
+    links[i].addEventListener('mouseover', () => {
+      cursor.classList.add('active')
+      follower.classList.add('active')
+    })
+
+    links[i].addEventListener('mouseout', () => {
+      cursor.classList.remove('active')
+      follower.classList.remove('active')
+    })
+
+  }
+
+  body.addEventListener('mouseout', () => {
+    cursor.classList.add('hidden')
+    follower.classList.add('hidden')
+  })
+
+})
