c6c853071f90280bf13fa4349e5d7009aec17a1f
Kseniia P <ksenia.pidopryhora@gmail.com>
2023-01-10 05:15:17 +0200

    update responsive

--- scripts/form.js
+++ scripts/form.js
@@ -8,8 +8,6 @@ output.innerHTML = range.value;
 range.oninput = function () {
   output.innerHTML = this.value;
 
-  // console.log(inputDollar.textContent)
-  // console.log(typeof Number(this.value))
   if (this.value === '25000') {
     output.innerHTML = ''
     warning.innerHTML = 'final project price is more than $ 25000'
@@ -24,12 +22,11 @@ range.oninput = function () {
     warning.innerHTML = ''
     inputDollar.innerHTML = ''
     inputDollar.innerHTML = `$ ${this.value}`
-    console.log(this.value)
   }
 
 }
 
-
+//changes range color depending on its value
 range.addEventListener('input', function () {
   const valueWidth = this.value;
   this.style.background = `linear-gradient(to right, var(--whiteText) 0%, var(--whiteText) ${valueWidth / 250}%, rgba(255, 255, 255, 0) ${valueWidth / 250}%, rgba(255, 255, 255, 0) 100%)`
