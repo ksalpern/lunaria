0b3acfcd4744e810bd0f26ae46a2542837f0a25f
Kseniia P <ksenia.pidopryhora@gmail.com>
2023-01-10 03:44:32 +0200

    add form

--- scripts/form.js
+++ scripts/form.js
@@ -1,14 +1,36 @@
 let range = document.getElementById("myRange");
 let output = document.getElementById("rangeValue");
+let inputDollar = document.getElementById("dollar")
+let warning = document.getElementById("warning")
+
 output.innerHTML = range.value;
 
 range.oninput = function () {
   output.innerHTML = this.value;
+
+  // console.log(inputDollar.textContent)
+  // console.log(typeof Number(this.value))
+  if (this.value === '25000') {
+    output.innerHTML = ''
+    warning.innerHTML = 'final project price is more than $ 25000'
+    inputDollar.innerHTML = ''
+    inputDollar.innerHTML = `More than $ ${this.value}`
+  } else if (Number(this.value) < 500) {
+    output.innerHTML = ''
+    inputDollar.innerHTML = `$ ${this.value}`
+    warning.innerHTML = 'start from $ 500'
+  } else {
+    output.innerHTML = ''
+    warning.innerHTML = ''
+    inputDollar.innerHTML = ''
+    inputDollar.innerHTML = `$ ${this.value}`
+    console.log(this.value)
+  }
+
 }
 
-const progress = document.querySelector('.myRange');
 
-progress.addEventListener('input', function () {
-  const value = this.value;
-  this.style.background = `linear-gradient(to right, #82CFD0 0%, #82CFD0 ${value / 250}%, #fff ${value / 250}%, white 100%)`
+range.addEventListener('input', function () {
+  const valueWidth = this.value;
+  this.style.background = `linear-gradient(to right, var(--whiteText) 0%, var(--whiteText) ${valueWidth / 250}%, rgba(255, 255, 255, 0) ${valueWidth / 250}%, rgba(255, 255, 255, 0) 100%)`
 })
