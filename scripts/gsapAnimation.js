e00c3e28d2e00b1af1a42693ec5a2b68f8577d93
Kseniia P <ksenia.pidopryhora@gmail.com>
2023-01-10 12:07:22 +0200

    gsap animation

--- scripts/gsapAnimation.js
+++ scripts/gsapAnimation.js
@@ -1,5 +1,46 @@
-// const tl = gsap.timeline({ defaults: { duration: .5 } });
-// tl.from('.message1', { opacity: 0, y: 30 })
-//   .from('.message2', { opacity: 0, y: 30 })
-//   .from('.banner__message button', { opacity: 0, y: 30 })
-//   .add(btnAnimation);
+// let blueCircle = document.querySelector('.blue')
+// let main = document.querySelector('.main')
+// console.log(blueCircle)
+// console.log(main)
+// gsap.timeline({
+//   scrollTriger:{
+//     trigger: '.main',
+//     start: 'center center',
+//     end:'bottom top',
+//     markers: true
+//   }
+// })
+// .from(blueCircle, {x: innerWidth + 100})
+// document.addEventListener("DOMContentLoaded", function () {
+//   gsap.utils.toArray(".blue").forEach((box, i) => {
+//     ScrollTrigger.create({
+//       trigger: '.main',
+//       start: "top c",
+//       pin: false,
+//       pinSpacing: true,
+//       snap: true,
+//       markers: true
+//     });
+//   });
+// });
+
+gsap.registerPlugin(ScrollTrigger)
+const trig = {
+  trigger: '.main',
+  start: 'top center',
+  scrub: true,
+  toggleActions: 'restart none none none'
+  // toggleActions: 'restart reverse reverse reverse'
+}
+gsap.to('.blue', {
+  scrollTrigger: trig,
+  x: '-60%',
+  y: '-5%', 
+  duration: 20
+})
+gsap.to('.yellow', {
+  scrollTrigger: trig,
+  x: '-90%', 
+  y: '-5%',
+  duration: 20
+})
